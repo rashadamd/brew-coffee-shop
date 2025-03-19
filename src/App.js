1511f72dcaf13778cb85b9c5f62bebcc4a9c2c62
rashadamd <nameisrashad@gmail.com>
2025-03-20 00:25:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import HeroSection from './components/HeroSection/HeroSection';
-/* 
 import AboutUs from './components/AboutUs/AboutUs';
+import TextMarquee from './components/TextMarquee/TextMarquee';
+
+/* 
 import WhyChooseUs from './components/WhyChooseUs/WhyChooseUs';
 import Pricing from './components/Pricing/Pricing';
 import Footer from './components/Footer/Footer'; */
@@ -14,8 +16,10 @@ function App() {
     <div className="App">
       <Header />
       <HeroSection />
-      {/* 
+      <TextMarquee text="THIS IS A TEST MARQUEE *  SCROLLING TEXT EXAMPLE *  ENJOY! " />
       <AboutUs />
+      {/* 
+      
       <WhyChooseUs />
       <Pricing />
       <Footer /> */}
